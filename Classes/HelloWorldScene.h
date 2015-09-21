ee9cd478a87bf2dbf71433e6583a5ea33090401b
forgetmenot256 <SHiNYA@Shinya-and-Mac-Book.local>
2015-09-22 00:45:36 +0900

    こみったん

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -9,10 +9,10 @@ public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
-    
+
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
-    
+
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 };
